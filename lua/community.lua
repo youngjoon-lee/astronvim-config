f6eeca809929b50c6b8ba8f08e73f04a28dc389a
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2024-04-02 00:28:19 +0900

    update to v4 official release

--- lua/community.lua
+++ lua/community.lua
@@ -6,8 +6,7 @@
 
 ---@type LazySpec
 return {
-  -- TODO: Remove branch v4 on release
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  { "AstroNvim/astrocommunity" },
   { import = "astrocommunity.recipes.vscode" },
   { import = "astrocommunity.git.openingh-nvim" },
   { import = "astrocommunity.git.octo-nvim" },
