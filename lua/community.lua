3e71173c84b8143c2528ad35883be0c63d50be3b
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2025-04-05 15:56:54 +0900

    add community modules

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -7,6 +5,23 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.recipes.vscode" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  -- Git
+  { import = "astrocommunity.git.openingh-nvim" },
+  { import = "astrocommunity.git.octo-nvim" },
+  -- AI
+  { import = "astrocommunity.editing-support.chatgpt-nvim" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.avante-nvim" },
+  -- Language Packs
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.python-ruff" },
+  ---- for formatting with organize-import in python-ruff
+  { import = "astrocommunity.editing-support.conform-nvim" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.proto" },
   -- import/override with your plugins folder
 }
